835110d0cfe6c287f8365eb335759b888f95078a
Stasys Meclazcke <aeip@live.com>
2020-10-22 15:37:34 -0500

    fixed toggle

--- src/components/Song.js
+++ src/components/Song.js
@@ -4,21 +4,29 @@ import './Song.scss'
 export const Song = (props) => {
     
 	return (
-		<div className='song-body'>	
+		<div className='song-body'>
 			<div className='title-artist-time'>
-			    <div className='title-artist'>
-			        <h3>{props.song.title}</h3>
-                    <h5>{props.song.artist}</h5>
-			    </div>
-                <p>{props.song.time}</p>
+				<div className='title-artist'>
+					<h3>{props.song.title}</h3>
+					<h5>{props.song.artist}</h5>
+				</div>
+				<p>{props.song.time}</p>
 			</div>
 			<div className='buttons'>
-                <button onClick={() => {
-                    props.selectSong(props.song);
-                    props.history.push('/edit')}}>Edit</button>
-				<button onClick={() => {
-                    props.deleteSong(props.song);
-                }}>Delete</button>
+				<button
+					onClick={() => {
+						props.selectSong(props.song);
+						props.history.push('/edit');
+					}}>
+					Edit
+				</button>
+				<button
+					onClick={() => {
+						props.deleteSong(props.song);
+					}}>
+					Delete
+				</button>
+				<button onClick={() => props.handleToggle(props.song)}>Favorite</button>
 			</div>
 		</div>
 	);
