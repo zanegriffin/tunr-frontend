22abbde5c14c11c68e80a6132d72be0ef49750b5
Woodrow Thompson <woodrow.thompson.iii@gmail.com>
2020-10-22 15:27:24 -0400

    initial styling work

--- src/components/AddNewSong.js
+++ src/components/AddNewSong.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import './AddNewSong.scss'
 
 export const AddNewSong = (props) => {
     return(
-        <div>
+        <div className='new-song-body'>
             <h1>ADD A NEW SONG</h1>
-            <h3>TITLE</h3>
-            <input type='text'/> <br/>
-            <h3>ARTIST</h3>
-            <input type='text'/> <br/>
-            <h3>TIME</h3>
-            <input type='text'/> <br/>
+            <div className='input-boxes'>
+                <h3>TITLE</h3>
+                <input type='text'/> <br/>
+                <h3>ARTIST</h3>
+                <input type='text'/> <br/>
+                <h3>TIME</h3>
+                <input type='text'/> <br/>
+            </div>
             <button>ADD A NEW SONG</button>
         </div>
     )
