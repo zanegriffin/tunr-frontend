2adbdd5291ca7b4487e698f8353d11022958de37
Woodrow Thompson <woodrow.thompson.iii@gmail.com>
2020-10-22 16:20:25 -0400

    scss edits again

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import './Song.scss'
 
 export const Song = (props) => {
 	return (
-		<div>
-			<div>
-				<h3>{props.song.title}</h3>
-				<div>
-                    <button onClick={() => {
-                        props.selectSong(props.song);
-                        props.history.push('/edit')}}>Edit</button>
-					<button onClick={() => {
-                        props.deleteSong(props.song);
-                    }}>Delete</button>
-				</div>
+		<div className='song-body'>	
+			<div className='title-artist-time'>
+			    <div className='title-artist'>
+			        <h3>{props.song.title}</h3>
+                    <h5>{props.song.artist}</h5>
+			    </div>
+                <p>{props.song.time}</p>
 			</div>
-			<h5>{props.song.artist}</h5>
-			<p>{props.song.time}</p>
+			<div className='buttons'>
+                <button onClick={() => {
+                    props.selectSong(props.song);
+                    props.history.push('/edit')}}>Edit</button>
+				<button onClick={() => {
+                    props.deleteSong(props.song);
+                }}>Delete</button>
+			</div>
 		</div>
 	);
 };
