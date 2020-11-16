6cf439add9a6bf27e7fe9a3e0e82fe1a4a1b832d
Zane <zanebgriffin@gmail.com>
2020-11-16 13:54:12 -0600

    files with correct names

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Header = (props) => {
+    return(
+        <div className='header-body'>
+            <header>
+                <h1>TUNR.</h1>
+                <h3>FOR ALL YOUR PLAYLIST NEEDS</h3>
+            </header>
+            <div className='header-border'></div>
+        </div>
+        
+    )
+}
